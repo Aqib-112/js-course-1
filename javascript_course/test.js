30336b628f6f194c768f06796987a64d258a4a1b
M . AQiB <aqibm6918@gmail.com>
2024-10-25 13:02:29 +0000

    data-types

--- javascript_course/test.js
+++ javascript_course/test.js
@@ -1,1 +1,28 @@
+
+/* print */
+
 console.log("Aqib")
+
+/* Variables */
+
+const UserId= 1234
+let UserName = "saad"
+let UserCity =  "Okara"
+  
+console.log(UserId);
+
+/*Print more than one */
+
+console.table([UserName,UserCity])
+
+/* Data-Types */
+
+// numbers => 2 power raise to 53
+// bigInt 
+// string => ""
+// boolean => true/flase
+// null =>standalone value
+// undefined
+// symbol => unique
+
+// object
