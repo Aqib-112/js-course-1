82c5619a4757c03a726952092912885ec835fbb9
M . AQiB <aqibm6918@gmail.com>
2024-10-26 11:41:40 +0000

    conversion data-types

--- javascript_course/test.js
+++ javascript_course/test.js
@@ -1,19 +1,19 @@
 
 /* print */
 
-console.log("Aqib")
+// console.log("Aqib")
 
 /* Variables */
 
-const UserId= 1234
-let UserName = "saad"
-let UserCity =  "Okara"
+// const UserId= 1234
+// let UserName = "saad"
+// let UserCity =  "Okara"
   
-console.log(UserId);
+// console.log(UserId);
 
 /*Print more than one */
 
-console.table([UserName,UserCity])
+//console.table([UserName,UserCity])
 
 /* Data-Types */
 
@@ -26,3 +26,44 @@ console.table([UserName,UserCity])
 // symbol => unique
 
 // object
+
+
+//Data -Types conversion
+
+let score=33
+
+console.log( typeof score) //number
+
+let NewScore = "33"
+
+console.log(typeof NewScore) // string
+
+// convert 'typeof' from string into number
+
+let ValueInNumber = Number(NewScore)
+console.log(typeof ValueInNumber) // now it convert into number
+
+// if we have mixture of number and string like "333abc" and try to convert into number it shows type 
+// into number but when we print is shows value into NaN means Not a Number
+
+
+ // if we have boolen in our NewScore and convert into number it shows 1 for true and 0 for false
+
+ // conersion from boolean to number
+
+ let IsLoggedIn = 1
+ let booleanIsLoggedIn = Boolen(IsLoggedIn)
+ console.log( typeof IsLoggedIn) // change number into boolean
+ console.log( IsLoggedIn) // vlaue is True
+
+
+ // conersion from number to string
+
+ let someNumber= 33
+ let stringNumber = String(someNumber)
+console.log(typeof stringNumber) // chnage into string
+console.log(stringNumber) // value is  33 which is in string
+
+// 
+
+
